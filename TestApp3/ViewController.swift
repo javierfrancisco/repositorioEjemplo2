42bfd57353ce667d4aefb14c70b27401fed046c1
zenkiu <javierfrancisco@gmail.com>
2017-03-22 23:51:53 -0400

    Initial Commit

---
+++ TestApp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApp3
+//
+//  Created by zenkiu on 3/22/17.
+//  Copyright © 2017 zenkiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
